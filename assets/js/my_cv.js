415c2dc1c8f8f8afb6f55f5a9f51db0d9ea53b51
Ohakwe-Love <ohakwemuna@gmail.com>
2025-09-16 12:36:44 +0100

    Updated my Portfolio

--- assets/js/my_cv.js
+++ assets/js/my_cv.js
@@ -1,29 +1,31 @@
 function downloadPDF() {
-    const element = document.getElementById('cv-container');
-    const opt = {
-        margin: 0.5,
-        filename: 'Ohakwe_Love_Resume.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { 
-            scale: 2,
-            useCORS: true,
-            letterRendering: true
-        },
-        jsPDF: { 
-            unit: 'in', 
-            format: 'a4', 
-            orientation: 'portrait' 
-        }
-    };
+    // const element = document.getElementById('cv-container');
+    // const opt = {
+    //     margin: 0.5,
+    //     filename: 'Ohakwe_Love_Resume.pdf',
+    //     image: { type: 'jpeg', quality: 0.98 },
+    //     html2canvas: { 
+    //         scale: 2,
+    //         useCORS: true,
+    //         letterRendering: true
+    //     },
+    //     jsPDF: { 
+    //         unit: 'in', 
+    //         format: 'a4', 
+    //         orientation: 'portrait' 
+    //     }
+    // };
 
-    // Hide the download button before generating PDF
-    const downloadBtn = document.querySelector('.download-btn');
-    downloadBtn.style.display = 'none';
+    // // Hide the download button before generating PDF
+    // const downloadBtn = document.querySelector('.download-btn');
+    // downloadBtn.style.display = 'none';
 
-    html2pdf().set(opt).from(element).save().then(() => {
-        // Show the download button again after PDF generation
-        downloadBtn.style.display = 'block';
-    });
+    // html2pdf().set(opt).from(element).save().then(() => {
+    //     // Show the download button again after PDF generation
+    //     downloadBtn.style.display = 'block';
+    // });
+
+    window.print();
 }
 
 document.addEventListener('keydown', function(e) {
